69bce0c98763bbc8e623fdfd77187ff129110362
emchik <153514866+mv-Levi@users.noreply.github.com>
2026-07-26 20:40:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+BALANCE = {}
+
+@app.get("/balance")
+def get_balance(wallet_name:str | None = None):
+    if wallet_name is None:
+        return {"total_balance": sum(BALANCE.values())}
+    if wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{wallet_name}' is not found"
+        )
+    return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+
+@app.post("/wallets/{name}")
+def receive_money(name: str, amount: int):
+    if name not in BALANCE:
+        BALANCE[name] = 0
+    BALANCE[name] += amount
+
+    return {
+        "message": f"Added {amount} to {name}",
+        "wallet": name,
+        "new_balance": BALANCE[name]
+    }
